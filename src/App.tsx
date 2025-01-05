f2cd15aa2e5aceba947d9361ffd28191682e147a
Johann GUITTET <guittet.johann@gmail.com>
2025-01-05 17:09:14 +0100

    change name and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import DicteeApp from './components/dictee-app-updated'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 function App() {
   return (
     <div className="min-h-screen p-4 bg-gray-50">
+      <SpeedInsights />
       <DicteeApp />
     </div>
   )
